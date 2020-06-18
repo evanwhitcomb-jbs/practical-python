e8ea97482b04c2b253e9f36df4198fcafb318665
evanwhitcomb-jbs <evan.whitcomb@jacksonbrothers.com>
2020-06-18 13:09:01 -0500

    completed pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,18 @@
 # pcost.py
 #
 # Exercise 1.27
+
+total_cost = 0.0
+
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f).split(',')
+headers
+for line in f:
+    row = line.split(',')
+    shares = float(row[1])
+    price = float(row[2])
+    total_cost = total_cost + (price * shares)
+
+f.close()
+
+print(f'Total cost {total_cost}')
