cef9b725c25683464391ac733ef49bf4a9ae8614
evanwhitcomb-jbs <evan.whitcomb@jacksonbrothers.com>
2020-06-18 15:12:55 -0500

    fixed report.py

--- Work/report.py
+++ Work/report.py
@@ -12,7 +12,7 @@ def read_portfolio(filename):
         rows = csv.reader(f)
         next(rows)
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
+            holding = {'name' : row[0], 'shares' : int(row[1]), 'price' : float(row[2])}
             portfolio.append(holding)
     
     return portfolio
