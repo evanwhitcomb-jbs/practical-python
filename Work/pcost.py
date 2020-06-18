3d6a78df70073ae01d262ffb838248728d7052f4
evanwhitcomb-jbs <evan.whitcomb@jacksonbrothers.com>
2020-06-18 13:33:07 -0500

    turned pcost.py into function and added error handling

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,17 +2,22 @@
 #
 # Exercise 1.27
 
-total_cost = 0.0
 
-f = open('Data/portfolio.csv', 'rt')
-headers = next(f).split(',')
-headers
-for line in f:
-    row = line.split(',')
-    shares = float(row[1])
-    price = float(row[2])
-    total_cost = total_cost + (price * shares)
+def portfolio_cost(filename):
+    total_cost = 0.0
+    with open(filename, 'rt') as f:
+        headers = next(f).split(',')
+        headers
+        for line in f:
+            row = line.split(',')
+            try:
+                shares = float(row[1])
+                price = float(row[2])
+                total_cost = total_cost + (price * shares)
+            except ValueError:
+                print("Couldn't parse", line)
+    return total_cost
 
-f.close()
 
-print(f'Total cost {total_cost}')
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
