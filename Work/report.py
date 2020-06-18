21d7b4448ac51162f6f7635fa520ef8eb9ab98f3
evanwhitcomb-jbs <evan.whitcomb@jacksonbrothers.com>
2020-06-18 14:31:19 -0500

    made report.py use tuples

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,18 @@
 # report.py
 #
 # Exercise 2.4
+import sys
+import csv
+
+def read_portfolio(filename):
+    '''Reads portfolio file into tuples'''
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+    
+    return portfolio
