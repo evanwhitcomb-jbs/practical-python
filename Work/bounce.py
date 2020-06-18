45eeee7e664879b0e4988ee5837572962ebe87a5
evanwhitcomb-jbs <evan.whitcomb@jacksonbrothers.com>
2020-06-18 08:59:13 -0500

    completed bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bounce = 3 / 5
+count = 0
+
+while count < 10:
+    height = height * bounce
+    count = count + 1
+    print(count, round(height, ndigits=4))
