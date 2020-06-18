da61f6d3ad28700b6764eb9f63298b8a95aedb59
evanwhitcomb-jbs <evan.whitcomb@jacksonbrothers.com>
2020-06-18 14:21:08 -0500

    updated pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -5,16 +5,17 @@ import sys
 import csv
 
 def portfolio_cost(filename):
+    '''Computes the total cost (shares * price) of a portfolio file'''
     total_cost = 0.0
+
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
-        headers = next(rows)
-        headers
+        next(rows)
         for row in rows:
             try:
                 shares = int(row[1])
                 price = float(row[2])
-                total_cost = total_cost + (price * shares)
+                total_cost += price * shares
             except ValueError:
                 print("Couldn't parse", row)
     return total_cost
